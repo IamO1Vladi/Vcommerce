7f3d32c2e2f4d40350cb07325de4b755fc893587
IamO1Vladi <vladimir96930@gmail.com>
2023-07-27 11:33:06 +0300

    AddingHomePageImagesAndFixingNavMenu

---
+++ Vcommerce/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vcommerce.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public IActionResult Products()
+        {
+
+
+
+            return View();
+        }
+
+    }
+}
