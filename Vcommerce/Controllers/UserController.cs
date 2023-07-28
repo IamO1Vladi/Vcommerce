6a2e52638c6739bb0f5b6365635b2d9ee5b2e64c
IamO1Vladi <vladimir96930@gmail.com>
2023-07-28 23:56:42 +0300

    AddingLoginAndRegisterViews

---
+++ Vcommerce/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Vcommerce.Data.Models.Users;
+
+namespace Vcommerce.Web.Controllers
+{
+    public class UserController : Controller
+    {
+
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+
+    }
+}
