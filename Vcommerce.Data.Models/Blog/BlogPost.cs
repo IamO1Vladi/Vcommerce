d3d1d3044766cfb1fa2efe7f182c69bc51e385a5
IamO1Vladi <vladimir96930@gmail.com>
2023-07-28 11:32:08 +0300

    AddingBlogPostCommentsAndDataValidations

--- Vcommerce.Data.Models/Blog/BlogPost.cs
+++ Vcommerce.Data.Models/Blog/BlogPost.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static VCommerce.Common.DataBaseValidations.BlogPostValidations;
 
 namespace Vcommerce.Data.Models.Blog
 {
-    internal class Blog
+    public class BlogPost
     {
+
+        public BlogPost()
+        {
+            this.Id = Guid.NewGuid();
+            this.Comments = new HashSet<Comment>();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required] 
+        [MaxLength(MaxTitleLength)] 
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public DateOnly DatePosted { get; set; }
+
+        [Required]
+        [MaxLength(MaxImageUrlLength)]
+        public string PostMainPictureUrl { get; set; } = null!;
+
+        [MaxLength(MaxImageUrlLength)]
+        public string? PostMidSectionLeftImageUrl { get; set; }
+
+        [MaxLength(MaxImageUrlLength)]
+        public string? PostMidSectionRightImageUrl { get; set; }
+
+        [Required]
+        [MaxLength(MaxContextLength)]
+        public string Context { get; set; } = null!;
+
+        public virtual ICollection<Comment> Comments { get; set; }
+
     }
 }
--- Vcommerce.Data.Models/Blog/Comment.cs
+++ Vcommerce.Data.Models/Blog/Comment.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static VCommerce.Common.DataBaseValidations.CommentsValidations;
 
 namespace Vcommerce.Data.Models.Blog
 {
-    internal class Comments
+    public class Comment
     {
+
+        public Comment()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(MaxAvatarUrlLength)]
+        public string AvatarImageUrl { get; set; } = null!;
+
+        [Required]
+        [MaxLength(MaxDescriptionLength)]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public DateTime DatePosted { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(RelatedPost))]
+        public Guid RelatedPostGuid { get; set; }
+
+        public virtual BlogPost RelatedPost { get; set; }
+
+        //Creating a self-referencing relationship for replies logic
+
+        [ForeignKey(nameof(ParentComment))]
+        public Guid? ParentCommentId { get; set; }
+
+        public virtual Comment ParentComment { get; set; }
+
+        public virtual ICollection<Comment> Replies { get; set; }
+
+
+
     }
 }
