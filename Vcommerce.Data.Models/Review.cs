47c9a200e8018411dbbfc89f30eef7ba88ed5617
IamO1Vladi <vladimir96930@gmail.com>
2023-07-28 10:52:36 +0300

    AddingReviewTableAndDataValidation

---
+++ Vcommerce.Data.Models/Review.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Vcommerce.Data.Models.Users;
+using static VCommerce.Common.DataBaseValidations.ReviewsValidations;
+
+namespace Vcommerce.Data.Models;
+
+public class Review
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public ApplicationUser UserPosted { get; set; } = null!;
+
+    [Required]
+    public DateOnly DatePosted { get; set; }
+
+    [Required]
+    [MaxLength(MaxAvatarUrlLength)]
+    public string AvatarUrl { get; set; } = null!;
+
+    [Required]
+    [Range(minimum:MinRating,maximum:MaxRating)]
+    public int Rating { get; set; }
+
+    [MaxLength(MaxDescriptionLength)]
+    public string? Description { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(RelatedClothing))]
+    public Guid RelatedClothingIdGuid{ get; set; }
+
+    public Clothes RelatedClothing { get; set; } = null!;
+
+
+
+}
